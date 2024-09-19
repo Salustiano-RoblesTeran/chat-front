797b8b9e6c8b7bf3705c9d80b29f1a6e30c36b6e
Salustiano-RoblesTeran <saluroblesteran@gmail.com>
2024-09-19 13:11:22 -0300

    cambie el back para que este alojado en vercel

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
 
   useEffect(() => {
     if (nombreIngresado) {
-      socket = io('http://localhost:3000');
+      socket = io('https://chat-back-1tib.onrender.com');
 
       socket.on('connect', () => setIsConnected(true));
       socket.on('disconnect', () => setIsConnected(false));
